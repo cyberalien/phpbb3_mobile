5031389ee51505c25bb784d6d78c34ec3af5fb86
Vjacheslav Trushkin <cyberalien@gmail.com>
2012-08-09 01:37:50 +0300

    Arrows for popups

--- style/template/mobile.js
+++ style/template/mobile.js
@@ -169,7 +169,15 @@ var phpBBMobile = {
 			{
 				return;
 			}
+			var arrowUp = document.createElement('span'),
+				arrowDown = document.createElement('span');
+			arrowUp.className = 'arrow-up';
+			arrowUp.innerHTML = '&uarr;';
+			arrowDown.className = 'arrow-down';
+			arrowDown.innerHTML = '&darr;';
 			menuTrigger.setAttribute('href', 'javascript:void(0);');
+			menuTrigger.appendChild(arrowUp);
+			menuTrigger.appendChild(arrowDown);
 			phpBBMobile.setupMenu(element, menuTrigger, menuItem);
 		});
 	}
